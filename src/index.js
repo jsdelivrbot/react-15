52a760418be869e09abb0f46d2311d7c358db9ff
Paul Calinovici <calinovicipaul@gmail.com>
2017-02-05 20:12:08 +0200

    delivered the first component. Yehaa!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+import * as React from "react";
+import ReactDom from "react-dom";
 /*Create a new component. This component should produce some html*/
 //some HTML
 
+//const is doing the same thing as var
+//const means that is not gonna change
+//we return JSX a subset of html
+const App = () => {
+    return <div>Hi,dude!</div>;
+}
 
-//Take this component
+//Take this component's generated html and put it on the page
+ReactDom.render(<App/>, document.querySelector('.container'));
