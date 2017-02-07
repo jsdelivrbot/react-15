c9b9009648820c70e2b4d0c07d3c0f7b0afa3bc7
Paul Calinovici <calinovicipaul@gmail.com>
2017-02-07 16:56:17 +0200

    added video content in list item component

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -4,8 +4,24 @@
 import React from 'react';
 
 
-const VideoListItem = (props) => {
-    return <li>Video</li>;
+const VideoListItem = ({video}) => {
+    //the upper statement makes the same with the bottom statement
+    // const video = props.video;
+
+    const imgUrl = video.snippet.thumbnails.default.url;
+
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imgUrl}/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {videos: []};
-        YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+        YTSearch({key: API_KEY, term: 'lambada'}, videos => {
             this.setState({videos});
             //this.setState({videos:videos});
         });
