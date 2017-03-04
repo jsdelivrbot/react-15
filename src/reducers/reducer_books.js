d9f8f37d1396ccd577bb72d5fa796bde2f9743a9
Paul Calinovici <calinovicipaul@gmail.com>
2017-03-04 11:50:15 +0200

    added books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,18 @@
+/**
+ * Created by IrianLaptop on 2/9/2017.
+ */
+//generate application state
+export default
+function () {
+    return [
+        {title: 'Javascript'},
+        {title: 'Harry Potter'},
+        {title: 'The dark tower'},
+        {title: 'Eloquent Ruby'}
+    ];
+}
+/*
+*
+* Step1: create reducer
+* Step2: add it in rootReducer -> reducers/index.js with key, value
+* S*/
