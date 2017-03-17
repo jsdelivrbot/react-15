7dadf15d2875dec25d8e03dab55c8ba7b1da15ed
Paul Calinovici <calinovicipaul@gmail.com>
2017-03-17 16:56:37 +0200

    added a router, a form to post a post and validation for form

---
+++ src/components/posts_new.js
@@ -0,0 +1,52 @@
+/**
+ * Created by IrianLaptop on 3/16/2017.
+ */
+import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+import {createPost} from '../actions/index';
+
+class PostsNew extends Component {
+
+    render() {
+        const {fields:{title, categories, content}, handleSubmit} = this.props;// handleSubmit = this.props.handleSubmit
+        return (
+            <form onSubmit={handleSubmit(this.props.createPost)}>
+                <h3>Create a New Post</h3>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control" {...title}/>
+                    <div className="text-help">
+                        {title.touched ? title.error : ''}
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label>Categories</label>
+                    <input type="text" className="form-control" {...categories}/>
+                </div>
+                <div className="form-group">
+                    <label>Content</label>
+                    <textarea className="form-control" {...content}/>
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        )
+    }
+}
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = 'Enter a username';
+    }
+
+    return errors;
+}
+//connect: first arg is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+export default reduxForm({
+    form: 'PostNew',
+    fields: ['title', 'categories', 'content'],
+    validate
+}, null, {createPost})(PostsNew);
+
